205ca571d1c15dad42b44fa18d75bb13962a3e2b
John Wassef <johnwasef2000.@gmail.com>
2023-07-06 03:34:36 +0300

    Finishing Note App

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:notes_app/constants.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:notes_app/models/note_model.dart';
+import 'package:notes_app/simple_bloc_observer.dart';
 
-void main() {
-  runApp(NoteApp());
+import 'package:notes_app/views/notes_view.dart';
+
+import 'cubits/notes_cubit/notes_cubit.dart';
+
+void main() async {
+  await Hive.initFlutter();
+
+  Bloc.observer = SimpleBlocObserver();
+  Hive.registerAdapter(NoteModelAdapter());
+  await Hive.openBox<NoteModel>(kNotesBox);
+
+  runApp(const NotesApp());
 }
 
-class NoteApp extends StatelessWidget {
-  const NoteApp({super.key});
+class NotesApp extends StatelessWidget {
+  const NotesApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return BlocProvider(
+       create: (context) => NotesCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
+        home: const NotesView(),
+      ),
+    );
   }
 }
-
-
-
-
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:notes_app/main.dart';
 
-import 'package:note_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(NoteApp());
+    await tester.pumpWidget(NotesApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
